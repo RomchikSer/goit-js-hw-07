8f50af92a6a69aa2f87086b297cf948729a314bc
Roman Serdiuk <75645387+RomchikSer@users.noreply.github.com>
2021-05-26 22:40:58 +0200

    remove

---
+++ js/task-02.js
@@ -0,0 +1,27 @@
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+  ];
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+const ingredientsOptions = (options) => {
+  return options.map(option => {
+    const ingredientsItemEl = document.createElement('li');
+    ingredientsItemEl.classList.add('ingredients-item');
+    const itemTextEl = document.createElement('p');
+    itemTextEl.classList.add('ingredients-text');
+    itemTextEl.textContent = option;
+    ingredientsItemEl.appendChild(itemTextEl);
+    return ingredientsItemEl;
+  }); 
+};
+
+const elements = ingredientsOptions(ingredients);
+ingredientsRef.append(...elements);
+
+console.log(ingredientsRef);
